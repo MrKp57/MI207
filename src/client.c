dd83427716dc940014321b86eb6cef9073a9f160
arkamor <m.aucher@live.fr>
2019-11-04 14:05:29 +0100

    Very simple server and client

--- src/client.c
+++ src/client.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main(int argc, char **argv){    
-    for (int i=0; i<5; i++){
-        printf("I am the client");
+#include <sys/stat.h>
+
+#define PIPE_PATH "/tmp/pipe0"
+
+#define DEBUG 1
+
+void exit_if(int condition, const char *prefix){
+    if (condition){
+        perror(prefix);
+        exit(1);
     }
+}
+
+int main(int argc, char **argv){   
+    printf("----- Start client -----\n"); 
+    //exit_if(mkfifo(PIPE_PATH, 0666)==-1,"mkfifo"); // Create fifo file
+    #if DEBUG
+        printf("DEBUG : fifo file created\n"); 
+    #endif
+
+    int fd = open(PIPE_PATH, O_WRONLY);
+    exit_if(fd == -1, PIPE_PATH); // Open fifo file in read only
+    #if DEBUG
+        printf("DEBUG : fifo file opened\n");    
+    #endif
+
+    printf("file %s is open for write\n", PIPE_PATH);
+    char buffer[100];
+    int n = snprintf(buffer, sizeof(buffer),"I'm %d", getpid());
+    write(fd, buffer, n);
+    printf("end of write\n");
+
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,9 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int main(int argc, char **argv){    
-    for (int i=0; i<5; i++){
-        printf("I am the server");
+#include <sys/stat.h>
+
+#define PIPE_PATH "/tmp/pipe0"
+
+#define DEBUG 1
+
+void exit_if(int condition, const char *prefix){
+    if (condition){
+        perror(prefix);
+        remove(PIPE_PATH);
+        exit(1);
     }
+}
+
+int main(int argc, char **argv){
+
+    printf("----- Start server:%d -----\n", getpid());
+
+    exit_if(mkfifo(PIPE_PATH, 0666)==-1,"mkfifo"); // Create fifo file
+    #if DEBUG
+        printf("DEBUG : fifo file created\n"); 
+    #endif
+
+    int fd = open(PIPE_PATH, O_RDONLY);
+    exit_if(fd == -1, PIPE_PATH); // Open fifo file in read only
+
+    #if DEBUG
+        printf("DEBUG : fifo file opened\n");    
+    #endif
+
+    int n;
+    char buffer[100];
+    while((n = read(fd, buffer, sizeof(buffer)-1)) > 0) {
+        buffer[n] = 0;
+        printf("reader: \"%s\"\n", buffer);
+    }
+
+    exit_if(remove(PIPE_PATH)==-1,"remove");
+    
     return 0;
 }
