b416df8eae74dc5a2e578ee659e669e39c239812
Kp <kp@2kpi.fr>
2019-11-08 18:20:20 +0100

    Client add utils

--- src/client.c
+++ src/client.c
@@ -1,13 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <fcntl.h>
 
 #include <sys/stat.h>
 
-#define PIPE_PATH "/tmp/chat/0"
+#define PIPE_PATH "/tmp/chat"
+#define MAIN_PIPE PIPE_PATH "/0"
 
 #define DEBUG
+
+int fd = -1;
+
+static const char *signames[] = {
+    "SIGHUP",  "SIGINT",    "SIGQUIT", "SIGILL",   "SIGTRAP", "SIGABRT", "SIGEMT",  "SIGFPE",
+    "SIGKILL", "SIGBUS",    "SIGSEGV", "SIGSYS",   "SIGPIPE", "SIGALRM", "SIGTERM", "SIGURG",
+    "SIGSTOP", "SIGTSTP",   "SIGCONT", "SIGCHLD",  "SIGTTIN", "SIGTTOU", "SIGIO",   "SIGXCPU",
+    "SIGXFSZ", "SIGVTALRM", "SIGPROF", "SIGWINCH", "SIGINFO", "SIGUSR1", "SIGUSR2",
+};
+
+const char *signame(int signal){
+    if (signal >= SIGHUP && signal <= SIGUSR2)
+        return signames[signal - SIGHUP];
+    return "SIG???";
+}
+
+void send_disconnect(){
+    char buffer[100];
+    int n = snprintf(buffer, sizeof(buffer),"%d,18,igloo/igloo\\igloo", getpid());
+    write(fd, buffer, n);
+    #ifdef DEBUG
+        printf("DEBUG : disconnect sent\n");    
+    #endif
+}
+
+void exiting(){
+    /*if(fd > 0) {
+        send_disconnect(fd);
+    }*/
+    printf("----- Stop client -----\n");
+    char path_pid_pipe[100];
+    sprintf(path_pid_pipe,"%s/%d",PIPE_PATH,getpid());
+    exit_if(remove(path_pid_pipe)==-1,"remove");
+    exit(1);
+}
+
 
 void exit_if(int condition, const char *prefix){
     if (condition){
@@ -25,20 +63,30 @@ void send_hello(int fd){
     #endif
 }
 
-void exiting(){
-    printf("----- Stop client -----\n");
-    exit(0);
-}
 int main(int argc, char **argv){   
     printf("----- Start client:%d -----\n",getpid());
 
-    if (access(PIPE_PATH, F_OK)){ // Si le pipe 0 n'existe pas
+    struct sigaction sa = {
+        .sa_flags = 0,
+    };
+    sigemptyset(&sa.sa_mask);
+    sa.sa_handler = exiting;
+
+    exit_if(sigaction(SIGINT, &sa, NULL) == -1,"sigaction");
+
+    char path_pid_pipe[100];
+    sprintf(path_pid_pipe,"%s/%d",PIPE_PATH,getpid());
+    #ifdef DEBUG
+        printf("DEBUG :  FILE PATH =  %s\n", path_pid_pipe);
+    #endif
+    exit_if(mkfifo(path_pid_pipe, 0666)==-1,"mkfifo"); // Create pid chat file
+
+    if (access(MAIN_PIPE, F_OK)){ // Si le pipe 0 n'existe pas
         printf("Please launch server first !\n"); 
-        printf("----- Stop client -----\n");
-        exit(0);
+        exiting(0);
     }
 
-    int fd = open(PIPE_PATH, O_WRONLY);
+    fd = open(MAIN_PIPE, O_WRONLY);
     exit_if(fd == -1, "Pipe open"); // Open fifo file in read only
 
     #ifdef DEBUG
@@ -47,12 +95,8 @@ int main(int argc, char **argv){
 
     send_hello(fd);
 
-    //usleep(1000*1000);
-
-    //exiting();
-
     #ifdef DEBUG
-        printf("file %s is open for write\n", PIPE_PATH);
+        printf("file %s is open for write\n", MAIN_PIPE);
     #endif
     
     // Demande du message au client
@@ -70,7 +114,7 @@ int main(int argc, char **argv){
     int n = snprintf(buffer, sizeof(buffer),"%d,6,coucou", getpid());
     write(fd, buffer, n);
     printf("%d bytes sent : \"%s\"\n",n,buffer);
-    
  
     return 0;
+
 }
